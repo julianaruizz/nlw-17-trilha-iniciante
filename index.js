fb9accb2aefac71a5a5df5b0950c0dacfa5d13fa
juliana@gmail.com <julianafpruiz@gmail.com>
2024-09-15 18:43:47 -0300

    metas abertas

--- index.js
+++ index.js
@@ -60,12 +60,27 @@ const  metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas: " + realizadas.length,
         choices: [...realizadas]
     })
 }
 
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
 
+
+if(abertas.length==0){
+    console.log("Não existem metas abertas")
+    return
+}
+
+await select ({
+    message: "Metas abertas: " + abertas.length,
+    choices: [...abertas]
+})
+}
 const start = async () => {
     while(true){
         
@@ -85,6 +100,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -102,6 +121,10 @@ const start = async () => {
             case "realizadas":
                 await metasRealizadas()
                 break
+            case "abertas":
+                await metasAbertas()
+                break
+
             case "sair":
                 console.log('Até a próxima!')
                 return
@@ -109,4 +132,4 @@ const start = async () => {
     }
 }
 
-start();
+start()
